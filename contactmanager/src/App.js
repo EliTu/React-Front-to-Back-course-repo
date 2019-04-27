b57fd1a51a8c426f14deae6d89af32588e73f0b4
EliTu <eliad91@gmail.com>
2019-04-27 13:13:03 +0800

    finished lecture 15, created my first custom React component

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Contact from './componenets/Contact';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>The App Component</h1>
+			<Contact />
 		</div>
 	);
 }
